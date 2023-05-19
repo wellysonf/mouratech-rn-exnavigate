8aa39a7b4231dde2b0e2d23d4d5f8de1489988d2
W. Fernando <wellysonf@gmail.com>
2023-05-18 21:38:21 -0300

    Exemplo do visual

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native'; 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from './src/theme';
+
+import { Loading } from '@components/Loading';
+import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        { fontsLoaded ? <Players/> : <Loading/> }
+      </>
+    </ThemeProvider>
+  );
+}
